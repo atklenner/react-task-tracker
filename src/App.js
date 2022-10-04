31b5d34753b98924e390d44af2e87f70780b7737
Andrew Klenner <atklenner@gmail.com>
2022-10-04 15:15:55 -0700

    fetch tasks from json-server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000);
