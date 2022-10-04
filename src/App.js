48b908fb031776b0a10ec32e563613afca3a22e0
Andrew Klenner <atklenner@gmail.com>
2022-10-04 08:25:16 -0700

    create Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Appointment", day: "02/05/2022", reminder: true },
+    { id: 2, text: "Meeting", day: "02/06/2022", reminder: true },
+    { id: 3, text: "Get Together", day: "02/07/2022", reminder: false },
+  ]);
   return (
     <div className="container">
-      <Header title="Task Tracker"/>
+      <Header title="Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,9 @@
+export default function Tasks({ tasks }) {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+}
