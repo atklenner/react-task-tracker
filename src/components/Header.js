42738ef92827f85de3e90e84f50f8d7ec9d5f850
Andrew Klenner <atklenner@gmail.com>
2022-10-04 08:20:09 -0700

    add button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-export default function Header() {
+import Button from "./Button";
+import PropTypes from "prop-types";
+
+export default function Header({title}) {
+  const onClick = () => {
+    console.log("Click")
+  }
+
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
+}
+
+Header.defaultProps = {
+  title: "Task Tracker"
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
 }
